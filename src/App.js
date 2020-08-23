7ab89ee6f94c4c0dcdcb9469dcfe54d0fbf78b5e
danield413 <danieldiazc413@gmail.com>
2020-08-23 13:47:12 -0500

    primcommit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React , {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+//importacion del cliente de Axios
+import clienteAxios from './config/axios'
+
+//componentes
+import Pacientes from './components/Pacientes';
+import NuevaCita from './components/NuevaCita';
+import Cita from './components/Cita'
+
+
+function App() {
+
+  //State de la app
+  const [citas, guardarCitas] = useState([]);
+  const [consultar, guardarConsultar] = useState(true);
+
+  useEffect( () => {
+    if(consultar){
+      const consultarAPI = () => {
+        clienteAxios.get('/pacientes')
+          .then(respuesta => {
+            //colocar en el state el resultado
+            guardarCitas(respuesta.data);
+
+            //deshabilitar la consulta
+            guardarConsultar(false);
+  
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+      consultarAPI();
+    }
+  }, [consultar] );
+
+  return (
+    <Router>
+        <Switch>
+            <Route exact path="/"
+            component={ () => <Pacientes citas={citas}  /> }
+            />
+            <Route exact path="/nueva"
+            component={ () => <NuevaCita guardarConsultar={guardarConsultar} /> }
+            />
+            <Route exact path="/cita/:id"
+            render={(props) => {
+
+              const cita = citas.filter(cita => cita._id === props.match.params.id)
+
+              console.log(cita)
+              
+              return (
+                <Cita 
+                  cita = {cita[0]}
+                  guardarConsultar = {guardarConsultar}
+                />
+              )
+            }}
+            />
+        </Switch>
+    </Router>
+  );
+}
+
+export default App;
